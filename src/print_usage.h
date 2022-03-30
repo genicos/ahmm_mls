70b05089dfb59d5d99faf83572158a1163c12eee
Nicolas Ayala <nicoatnico.com@gmail.com>
2022-03-29 18:12:24 -0700

    Added model file help text to print usage

--- src/print_usage.h
+++ src/print_usage.h
@@ -7,8 +7,11 @@ void print_usage() {
     cerr << "\trequired:" << endl ;
     cerr << "\t\t-i [string]\t\tinput file name" << endl ;
     cerr << "\t\t-s [string]\t\tsample id and ploidy file" << endl ;
-    cerr << "\t\t-l [string]\t\tsite file" << endl ;
     cerr << "\t\t-m [float] [int]\t\tadmixture information with admixture fraction, time" << endl ;
+
+    cerr << "\tat least one required:" << endl;
+    cerr << "\t\t-l [string]\t\tsite file" << endl ;
+    cerr << "\t\t-M [string]\t\tmodel file" << endl ;
 
     cerr << "\toptional:" << endl ;
     cerr << "\t\t--help\t\t\tprint this help statement" << endl ;
--- src/read_cmd_line.h
+++ src/read_cmd_line.h
@@ -260,9 +260,9 @@ void cmd_line::read_cmd_line ( int argc, char *argv[] ) {
         exit(1) ;
     }
 
-    if ( use_site_file == false && use_model_file == false){
+    if ( use_site_file == false && use_model_file == false ){
         cerr << "\n\n\t\tERROR: must provide either site file or model file \n\n\t\t\t-l [path/to/site_file]\n\n" ;
-        cerr << "\t\t\t-M [path/to/model_file]\n\n";
+        cerr << "\t\t\t-M [path/to/model_file]\n\n" ;
         print_usage() ;
         exit(1) ;
     }
